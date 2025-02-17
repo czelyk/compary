8a449795b071f76d9ca540fd83d40803e8a94e49
CZELYK <ahmethassancelik@gmail.com>
2025-02-17 18:50:12 +0300

    url problem is fixed

--- amazon/code/main.py
+++ amazon/code/main.py
@@ -1,12 +1,11 @@
-import sys
 from scraper import scrape_amazon_product
 
 def main():
-    if len(sys.argv) > 1:
-        url = sys.argv[1]
+    url = input("Enter the Amazon product URL: ").strip()  # Take URL input from the user
+    if url:
         scrape_amazon_product(url)  # Scrape the product with the provided URL
     else:
-        print("Usage: python main.py <Amazon URL>")
+        print("Please provide a valid Amazon URL.")
 
 if __name__ == "__main__":
     main()
