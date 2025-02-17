9cb4b01b3bd3805744a42336bfd070d532f6e7a3
CZELYK <ahmethassancelik@gmail.com>
2025-02-17 18:43:49 +0300

    changes have been done for better structure

--- amazon/code/scraper.py
+++ amazon/code/scraper.py
@@ -12,7 +12,7 @@ def scrape_amazon_product(url):
 
     # Define directories
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-    RESULTS_DIR = os.path.join(BASE_DIR, "results")
+    RESULTS_DIR = os.path.join(BASE_DIR, "..", "results")  # Navigate up to 'results'
     os.makedirs(RESULTS_DIR, exist_ok=True)
 
     # Get a random user-agent
