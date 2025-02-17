068e361ac61e08769eb7ad16401130325b3a9b6e
CZELYK <ahmethassancelik@gmail.com>
2025-02-17 18:06:37 +0300

    first scraping memthods have been completed.

--- amazon/app.py
+++ amazon/app.py
@@ -1,5 +1,7 @@
 import re
 
+
+
 def is_amazon_link(url):
     # Regular expression pattern to match Amazon URLs, including Amazon Turkey
     pattern = r"https?://(?:www\.)?amazon\.(com|co\.[a-z]{2,3}|ca|co\.uk|de|fr|it|es|in|jp|mx|com\.br|com\.au|co\.jp|cn|com\.tr)/"
@@ -18,3 +20,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
